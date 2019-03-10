1fdb2c34e2bff28160343323925790daf2ffe6d0
Harsh Pandya <pandyaharsh43@gmail.com>
2019-03-10 14:39:14 -0500

    Understanding Babel

---
+++ indecision-app/public/scripts/app.js
@@ -0,0 +1,4 @@
+// var template = <p>This is JSX from app.js</p>;
+var template = React.createElement("p", null, "This is JSX from app.js");
+var appRoot = document.getElementById("app");
+ReactDOM.render(template, appRoot);
